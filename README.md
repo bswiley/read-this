7ace9a595e5f53d3f9bca995ffcb9b784e7a032e
bswiley <bswiley@gmail.com>
2023-05-06 02:52:56 -0500

    Fixed what was lacking

--- README.md
+++ README.md
@@ -1,14 +1,24 @@
-#Read-This
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+# Read-This
 
 ## Description
-The purpose of this project is to make writing README.md files easier.  I found that I had problems writing README.md files mainly because I always was trying to think about how they would turn out in advance of writing them.  The anxiety made it all the harder to write them.  As such I thought that this application would make it esaier to write them because all I'd have to do was answer a few questions and the README.md would be written based on those questions.  I wouldn't have to spend time thinking so much about how to compose the README.md.  This application solved that problem.  I made it so that all you have to do is answer a few questions and after they are over, it will write the README.md for me. 
+It does this . . . 
 
+
+## Table of Contents
+[Installation](#Installation)
+[Usage](#Usage)
+[License](#License)
+[Questions](#Questions)
 ## Installation
-To use this application, Node.js needs to be installed as well as the dependencies inquireer@8.2.4 and fs.
+Install it like this . . .  
 
 ## Usage
-Once the environment is installed all one has to do is type "node index.js" and the application will run
+Use it thise way . . . . 
 
 ## License
-MIT
-git
+This project has a/n MIT License
+
+## Questions
+The project is one of my projects which can be reached at github.com/bswiley.If you have questions, please email me at bswiley@gmail.com.
--- index.js
+++ index.js
@@ -3,11 +3,19 @@ const inquirer = require("inquirer");
 
 inquirer
   .prompt([
-   {
-    type: 'statement',
-    message: "Answer the following questions in full sentences in a way that your responses are complete and don't appear to be answers to questions (hit enter when ready)",
-    name: "none",
-
+    {
+      type: 'statement',
+      message: "Answer the following questions in full sentences (when possible) in a way that your responses are complete and don't appear to be answers to questions (hit enter when ready)",
+      name: "none",
+     },
+     {
+        type: 'input',
+        message: 'What is your Github username?',
+        name: 'github',
+      },{
+    type: 'input',
+    message: "What is your email address?",
+    name: "email",
    },
    {
       type: 'input',
@@ -16,7 +24,7 @@ inquirer
     },
     {
     type: 'input',
-    message: "Briefly write a description of yoru project so that potential users know what it does",
+    message: "Briefly write a description of your project so that potential users know what it does",
     name: 'description',
     },
    {
@@ -31,63 +39,129 @@ inquirer
   },
   {
   type: 'input',
-  message: 'List your collaboratores if there are any',
+  message: 'Describe ways that viewers can make contributions if any',
   name: 'collaboration',
   },
   {
-            type: 'input',
-            message: 'What liscens are you using on the project?',
-            name: 'license',
-          }])
-      .then((response)=>
-{const{title, description, installation, usage, collaboration, license} = response;
+    type: 'input',
+    message: 'Describe the tests if there are any',
+    name: 'tests',
+    },
+  {
+  type: 'list',
+  message: 'What liscens are you using on the project?',
+  name: 'license',
+  choices: ["None","Apache 2.0 License","Boost Software License 1.0","BSD 3-Clause License","BSD 2-Clause License","Creative Commons Zero v1 Univeral","GNU Affero General Public License v3.0","GNU General Public License v3.0","GNU General Public License V2.0","GNU Lesser General Public License v2.1","MIT License","Mozilla Public License 2.0","The Unlicense"]
+ },
+ {
+  type: 'input',
+  message: 'What should someone do if they have Questions?',
+  name: 'questions',
+  }
+])
+      .then((response)=>{
+ const {github, email, title, description, installation, usage, collaboration, license, tests, questions} = response;
        console.log(response)
-      writeFile(title, description, installation, usage, collaboration, license)})
+     writeFile(github, email, title, description, installation, usage, collaboration, license, tests, questions)})
 
-function writeFile(title, description, installation, usage, collaboration, license){
-  if (title){
-  fs.writeFile('./README.md', `#${title}\n`, err =>
-  {if (err) {
-    console.error(err);
-  node}});}
-  else if (!title){
-    fs.writeFile('./README.md', `#\n`, err =>
-  {if (err) {
-  console.error(err);
-  node}});}
-  if (description){
-  fs.appendFile('./README.md', `\n## Description\n${description}\n`, err =>
-  {if (err) {
-  console.error(err);
-  node}});} 
-  else if (!description){}
-  if (installation){
-  fs.appendFile('./README.md', `\n## Installation\n${installation}\n`, err =>
-  {if (err) {
-  console.error(err);
-  node}});} 
-  else if (!installation){
-  }
-  if (usage){
-  fs.appendFile('./README.md', `\n## Usage\n${usage}\n`, err =>
-  {if (err) {
-  console.error(err);
-  node}});}
-  else if (!usage){
-  }
-  if (collaboration){
-  fs.appendFile('./README.md', `\n## Collaboration\n${collaboration}\n`, err =>
-  {if (err) {
-  console.error(err);
-  node}});} 
-  else if (!collaboration){
-  }
-  if (license){
- fs.appendFile('./README.md', `\n## License\n${license}\n`, err =>
- {if (err) {
- console.error(err);
- node}});} 
- else if (!license){
- }
+function writeFile(github, email, title, description, installation, usage, collaboration, license, tests, questions){
+switch (license) {
+case 'Apache 2.0 License':
+fs.writeFile("./README.md", "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)", err  =>
+err ? console.error(err): console.log (""));
+break;
+case "Boost Software License 1.0":
+fs.writeFile("./README.md", "[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)", err  =>
+err ? console.error(err): console.log (""));
+break;
+case "BSD 3-Clause License":
+fs.writeFile("./README.md", "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)", err  =>
+err ? console.error(err): console.log (""));
+break;
+
+case "BSD 2-Clause License":
+fs.writeFile("./README.md", "[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)", err  =>
+err ? console.error(err): console.log (""));
+break;
+
+case "Creative Commons Zero v1 Univeral":
+fs.writeFile("./README.md", "[![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)](http://creativecommons.org/publicdomain/zero/1.0/)", err  =>
+err ? console.error(err): console.log (""));
+break;
+
+case "GNU Affero General Public License v3.0":
+fs.writeFile("./README.md", "[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)", err  =>
+err ? console.error(err): console.log (""));
+break;
+
+case "GNU General Public License v3.0":
+fs.writeFile("./README.md", "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)", err  =>
+err ? console.error(err): console.log (""));
+break;
+  
+case "GNU General Public License V2.0":
+fs.writeFile("./README.md", "[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)", err  =>
+err ? console.error(err): console.log (""));
+break;
+case "GNU Lesser General Public License v2.1":
+fs.writeFile("./README.md", "[![License](https://img.shields.io/badge/License-LGPL_v2.1-blue.svg)](https://www.gnu.org/licenses/old-licenses/lgpl-2.1)", err  =>
+err ? console.error(err): console.log (""));
+break;
+
+case "MIT License":
+fs.writeFile("./README.md", "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)", err  =>
+err ? console.error(err): console.log (""));
+break;
+
+case "Mozilla Public License 2.0":
+fs.writeFile("./README.md", "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)", err  =>
+err ? console.error(err): console.log (""));
+break;
+case "The Unlicense":
+fs.writeFile("./README.md", "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)", err  =>
+err ? console.error(err): console.log (""));
+break;
+  
+default:
+fs.writeFile("./README.md", " ", err  =>
+err ? console.error(err): console.log (""));
+break;
+}  
+if (title){
+fs.appendFile('./README.md', `\n\n# ${title}\n`, err =>
+err ? console.error(err): console.log("Title written"));}
+else if (!title){
+fs.appendFile('./README.md', `\n\n#\n`, err =>
+err ? console.error(err): console.log("Missing title not written"));}
+if (description){
+fs.appendFile('./README.md', `\n## Description\n${description}\n`, err =>
+err ? console.error(err): console.log("Description written"));}
+else if (!description){}
+fs.appendFile('./README.md', `\n\n## Table of Contents${installation ?"\n[Installation](#Installation)":""}${usage ?"\n[Usage](#Usage)":""}${collaboration ?"\n[Contributing](#Contributing)":""}${tests ?"\n[Tests](#Tests)":""}${license ?"\n[License](#License)":""}${questions || github || email ?"\n[Questions](#Questions)":""}`, err =>
+err ? console.error(err): console.log("Table of Contents Written"));
+if (installation){
+fs.appendFile('./README.md', `\n## Installation\n${installation}\n`, err =>
+err ? console.error(err): console.log("Installation written"));}
+else if (!installation){}
+if (usage){
+fs.appendFile('./README.md', `\n## Usage\n${usage}\n`, err =>
+err ? console.error(err): console.log("Usage written"));}
+else if (!usage){}
+if (collaboration){
+fs.appendFile('./README.md', `\n## Contributing\n${collaboration}\n`, err =>
+err ? console.error(err): console.log("Collaboration written"));}
+else if (!collaboration){}
+if (tests){
+ fs.appendFile('./README.md', `\n## Tests\n${tests}\n`, err =>
+ err ? console.error(err): console.log (""));}
+ else if (!license){}
+ if (license){
+ fs.appendFile('./README.md', `\n## License\nThis project has a/n ${license}\n`, err =>
+ err ? console.error(err): console.log("License written"));}
+ else if (!license){}
+ if (questions || github || email){
+ fs.appendFile('./README.md', `\n## Questions\n${questions ?`${questions}`:""}${github ?`This project can be accesed at github.com/${github}.  `:""}${email ?`If you have questions, please email me at ${email}.`:""}`, err =>
+ err ? console.error(err): console.log (""));}
+ else if (!questions || !github || !email){}
  console.log("Your README.md is ready!")
-}
+ }
